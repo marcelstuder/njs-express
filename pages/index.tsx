03af17fadcd053733cca5f6fba3437bf787ff22d
Marcel Studer <marcel.studer@gmx.at>
2021-03-31 21:45:38 +0200

    Fix error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,32 @@
 import { useEffect, useState } from 'react';
 import jwt from 'jsonwebtoken';
 
-const [ username, setUserName ] = useState<string>('');
-const [ password, setPassword ] = useState<string>('');
-const [ message, setMessage ] = useState<string>('You are not logged in.');
-
-async function submitForm() {
-  const res = await fetch('/api/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ username, password })
-  }).then((t) => t.json());
-
-  const token = res.token;
-
-  if (token) {
-    const json = jwt.decode(token) as { [key: string]: string };
-    console.log(json);
-    setMessage(`Welcome, ${json.username}, you are ${json.admin ? 'an admin' : 'not an admin'}. `);
-  }
-  else {
-    setMessage('Ooops! Something went terribly wrong.');
-  }
-}
-
 export default function Home() {
+  const [ username, setUserName ] = useState<string>('');
+  const [ password, setPassword ] = useState<string>('');
+  const [ message, setMessage ] = useState<string>('You are not logged in.');
+
+  async function submitForm() {
+    const res = await fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    }).then((t) => t.json());
+
+    const token = res.token;
+
+    if (token) {
+      const json = jwt.decode(token) as { [key: string]: string };
+      console.log(json);
+      setMessage(`Welcome, ${json.username}, you are ${json.admin ? 'an admin' : 'not an admin'}. `);
+    }
+    else {
+      setMessage('Ooops! Something went terribly wrong.');
+    }
+  }
+
   return (
     <div>
       <h1>{message}</h1>
